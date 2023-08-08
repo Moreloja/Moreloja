ff2a3e110988a757e4af151ac042189f9b3b0b96
Ma <101021254+CodeWithMa@users.noreply.github.com>
2023-08-08 12:51:42 +0200

    fix process.env access

--- libs/api/configurations/src/lib/pictrs.configuration.ts
+++ libs/api/configurations/src/lib/pictrs.configuration.ts
@@ -3,9 +3,9 @@ import { ConfigType, registerAs } from '@nestjs/config';
 
 export const pictrsConfiguration = registerAs('pictrs', () => {
   return {
-    protocol: process.env.PICTRS_PROTOCOL || 'http',
-    host: process.env.PICTRS_HOST || 'localhost',
-    port: Number(process.env.PICTRS_PORT) || '8080',
+    protocol: process.env['PICTRS_PROTOCOL'] || 'http',
+    host: process.env['PICTRS_HOST'] || 'localhost',
+    port: Number(process.env['PICTRS_PORT']) || '8080',
     get domain() {
       return `${this.protocol}://${this.host}:${this.port}`;
     },
