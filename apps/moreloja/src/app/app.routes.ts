a88461c37ccebe44c37e52b1b7b681db520c1ce0
Ma <101021254+CodeWithMa@users.noreply.github.com>
2023-08-11 17:32:17 +0200

    Add title to routing paths

--- apps/moreloja/src/app/app.routes.ts
+++ apps/moreloja/src/app/app.routes.ts
@@ -1,28 +1,36 @@
 import { Route } from '@angular/router';
 
+const title = 'Moreloja';
+
 export const appRoutes: Route[] = [
     {
         path: 'album/:mbidAlbumInput',
+        title: `${title} - Album`,
         loadComponent: () => import('./album/album.component')
     },
     {
         path: 'albums',
+        title: `${title} - Albums`,
         loadComponent: () => import('./albums/albums.component')
     },
     {
         path: 'artist/:mbidAlbumArtistInput',
+        title: `${title} - Artist`,
         loadComponent: () => import('./artist/artist.component')
     },
     {
         path: 'artists',
+        title: `${title} - Artists`,
         loadComponent: () => import('./artists/artists.component')
     },
     {
         path: 'song',
+        title: `${title} - Song`,
         loadComponent: () => import('./song/song.component')
     },
     {
         path: 'songs',
+        title: `${title} - Songs`,
         loadComponent: () => import('./songs/songs.component')
     }
 ];
